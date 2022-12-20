025b5eeeb739c7378ddadb84381dff74048b3858
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-20 18:31:35 -0300

    feat: welcome home page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '../../components'
+import { Container, InformationWrapper } from './styles'
 
 const WelcomePage = () => {
-  return <div>WelcomePage</div>
+  const navigate = useNavigate()
+
+  const handleClick = () => navigate('/home')
+
+  return (
+    <Container>
+      <InformationWrapper>
+        <h2>Welcome to</h2>
+
+        <h1>
+          Bed <br />
+          Buddy
+        </h1>
+
+        <Button onClick={handleClick}>Ready!</Button>
+
+        <p>The best hotel bookings in the century to accompany your vacation</p>
+      </InformationWrapper>
+    </Container>
+  )
 }
 
 export default WelcomePage
---
+++ src/pages/Welcome/styles.tsx
@@ -0,0 +1,62 @@
+import styled, { keyframes } from 'styled-components'
+
+const AppearAnimation = keyframes`
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+`
+
+export const Container = styled.main`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: flex-end;
+  background-image: url('/images/hotel-cover.jpeg');
+  background-size: cover;
+
+  @media (min-width: 768px) {
+    background-position: bottom;
+  }
+`
+
+export const InformationWrapper = styled.div`
+  width: 100%;
+  height: 65%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  color: white;
+  background: linear-gradient(to top, black, transparent);
+  padding: 32px;
+
+  h2 {
+    font-size: 42px;
+    font-weight: 700;
+  }
+
+  h1 {
+    font-size: 96px;
+    font-weight: 700;
+    color: #1ab65c;
+  }
+
+  button {
+    margin: 0 auto;
+    animation: ${AppearAnimation} 2s ease-in;
+  }
+
+  @media (min-width: 768px) {
+    button {
+      margin: 0;
+    }
+  }
+`
