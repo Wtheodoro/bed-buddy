4c09c2109ea0f4fc84381a6d8eb2f02f7df4a0b6
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-22 17:17:07 -0300

    fix: my booking page size

--- src/pages/MyBooking/styled.tsx
+++ src/pages/MyBooking/styled.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
   padding: 20px;
   display: flex;
   flex-direction: column;
