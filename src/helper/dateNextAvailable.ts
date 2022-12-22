0f40671c3c5b71b89ad2bec140deaf2984d2f4a2
Walison Theodoro <theodoro.walison@gmail.com>
2022-12-22 15:35:27 -0300

    fix: next date helper function

--- src/helper/dateNextAvailable.ts
+++ src/helper/dateNextAvailable.ts
@@ -2,19 +2,24 @@ import moment from 'moment'
 
 const dateNextAvailable = (bookedDates: any[]) => {
   if (!bookedDates.length) return new Date()
+  const sortedDates = bookedDates.sort((a, b) => a - b)
 
   const now = new Date()
 
   let daysToSkipFromToday = 0
 
-  for (let i = 0; i < bookedDates.length; i++) {
+  for (let i = 0; i < sortedDates.length; i++) {
     const baseDate = new Date(
       Math.abs(now as any) + daysToSkipFromToday * (1000 * 3600 * 24)
     )
-    if (dateFormat(baseDate) !== dateFormat(bookedDates[i])) break
-
-    daysToSkipFromToday++
+    if (dateFormat(baseDate) === dateFormat(sortedDates[i]))
+      daysToSkipFromToday++
   }
+
+  console.log(daysToSkipFromToday)
+  console.log(
+    new Date(Math.abs(now as any) + daysToSkipFromToday * (1000 * 3600 * 24))
+  )
 
   return new Date(
     Math.abs(now as any) + daysToSkipFromToday * (1000 * 3600 * 24)
